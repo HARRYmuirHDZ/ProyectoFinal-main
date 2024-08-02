80e17f09ce0f7d4334954a24f21ed76bf6bd4e9d
HARRYmuirHDZ <pottermuir@gmail.com>
2024-08-01 20:39:54 -0600

    feat-entregable 5 1/08/2024

--- ProyectoFinal-main/js/main.js
+++ ProyectoFinal-main/js/main.js
@@ -3,7 +3,18 @@ import { MostrarHTML} from "/js/crearHTML.js"
 //componentes
 document.addEventListener("DOMContentLoaded", ev => {
     MostrarHTML()
+    MostarDatosConfiguraciones()
 })
 
 
 
+function MostarDatosConfiguraciones() {
+    //truco  !fetch
+    let url ;
+    fetch(url)
+    
+        .then(response => response.json())
+    
+        .then(data => console.log(data));
+
+}
