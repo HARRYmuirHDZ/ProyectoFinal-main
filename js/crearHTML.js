53462aca48edcb7a5b1d1c23e4ccf2c29dd9600b
HARRYmuirHDZ <pottermuir@gmail.com>
2024-08-05 15:29:31 -0600

    feat-Tarea JS 5

--- js/crearHTML.js
+++ js/crearHTML.js
@@ -1,13 +1,14 @@
 //Modularizar Exportar e importar
-
 ///0-
 export function MostrarHTML() {
   //
-  let navbar = document.getElementById("Navbar").innerHTML= CrearNavbar();
-  let footer= document.getElementById("Footer").innerHTML= CrearFooter();
+  let navbar = document.getElementById("Navbar").innerHTML = CrearNavbar();
+  let footer = document.getElementById("Footer").innerHTML = CrearFooter();
 
-  let Noticias = document.getElementById("Noticias").innerHTML= CrearTarjetaNoticias();
-  
+  let Noticias = document.getElementById("Noticias").innerHTML = CrearTarjetaNoticias();
+  let Tabla = document.getElementById("Tabla").innerHTML = CrearTabla();
+  let Banner1 = document.getElementById("Banner1").innerHTML = CrearBanner1();
+
 }
 
 //1-Navbar
@@ -78,10 +79,72 @@ function CrearTarjetaNoticias() {
   return html
 }
 
-function CrearParrafo(){
+function CrearParrafo() {
   let texto = "Genesis"
-  
-  let html =`<p>${texto} </p>`
+
+  let html = `<p>${texto} </p>`
 
   return html;
+}
+
+
+function CrearTabla() {
+  let html = `
+<!-- Tabla-->
+<div class="spaced-div">
+  <section class="banner banner-tarjetas-dos ">
+    <div class="container">
+
+      <!-- b4-form-inline
+  b4-form-group -->
+      <h2>Tabla Datos</h2>
+      <table class="table table-striped table-inverse table-responsive container col-6 text-white" id="Tabla">
+
+
+  <thead class="thead-inverse">
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+          
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>asas</td>
+            <td>asas</td>
+          
+          </tr>
+          
+        </tbody>
+
+
+
+
+      </table>
+  </section>
+`
+
+  return html;
+}
+
+
+function CrearBanner1() {
+  let html = `
+    <div class="spaced-div">
+      <div class="container">
+        <h2>Lorem ipsum dolor.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut qui laudantium id quam magni accusantium,
+          veritatis, ipsam labore, reprehenderit dolore repudiandae nemo sint deserunt! Suscipit facilis qui
+          inventore
+          consequatur fugit.</p>
+        <a href="#" class="boton">Más información <i class="fas fa-chevron-right"></i></a>
+      </div>
+    </div>
+
+
+
+  `
+
+  return html
 }
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 import { MostrarHTML } from "./crearHTML.js"
 import { MostarDatosEnConsola } from "/js/consultarJson.js"
+
+
 //componentes
 document.addEventListener("DOMContentLoaded", ev => {
     MostrarHTML();
     MostarDatosEnConsola ();
 
+
 });
 
+``
