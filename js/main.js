7d24d0a5778cc57e8e630979af217e7abd11ddf1
HARRYmuirHDZ <pottermuir@gmail.com>
2024-08-06 21:40:01 -0600

    feat-entregable 6 06/08/2024

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 import { MostrarHTML } from "/js/Global.js"
 import { ObtenerDatosDeConfiguracion, ObtenerDatosApiRick } from "/js/servicios_configuracion.js"
-
+import { MostrarCard } from "/js/Card.js"
 
 //componentes
 document.addEventListener("DOMContentLoaded", ev => {
     MostrarHTML();
-    //ObtenerDatosDeConfiguracion ();
+    ObtenerDatosDeConfiguracion ();
     ObtenerDatosApiRick();
+    MostrarCard();
 
-});
+})
 
