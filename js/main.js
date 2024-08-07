2557d7596de7f74040896d7927fd5b9e8fe5100f
HARRYmuirHDZ <pottermuir@gmail.com>
2024-08-06 21:01:15 -0600

    feat-entregable 5 06/08/2024

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { MostrarHTML } from "/js/Global.js"
-
+import { ObtenerDatosDeConfiguracion, ObtenerDatosApiRick } from "/js/servicios_configuracion.js"
 
 
 //componentes
 document.addEventListener("DOMContentLoaded", ev => {
     MostrarHTML();
     //ObtenerDatosDeConfiguracion ();
-
+    ObtenerDatosApiRick();
 
 });
 
--- js/servicios_configuracion.js
+++ js/servicios_configuracion.js
@@ -25,3 +25,35 @@ export const ObtenerDatosDeConfiguracion = async () => {
     }
 
 };
+
+
+export const ObtenerDatosApiRick= async () => {
+    const url = "https://rickandmortyapi.com/api/character"
+
+    try {
+
+        const response = await fetch(url);
+        if (response.status == "200") {
+        const data = await response.json();
+
+            console.warn(data.results);
+            return data.results
+
+        }else{
+            console.log("Error al consultar la API"+ response.status)
+        }
+        
+
+        
+
+    } catch (error) {
+
+
+        console.log("Error en la red "+ error);
+
+    }
+
+};
+
+
+
